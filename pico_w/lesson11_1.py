f73b646d6d93617c3ffa0210cf74a3fa83b9c7e4
Tommy Chou <tommy9567@gmail.com>
2024-01-26 09:17:08 +0800

    modify

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -1,4 +1,4 @@
-from machine import Pin
+from machine import Pin, ADC
 from tools import connect,reconnect
 import time
 import urequests
@@ -6,22 +6,45 @@ import urequests
 red_led = Pin(15,mode=Pin.OUT)
 btn = Pin(14,mode=Pin.PULL_DOWN)
 is_press = False
-#connect()
+connect()
+
+def getCurrentTime():
+    times_tuple = time.localtime()
+    currentTime = f'{times_tuple[0]}-{times_tuple[1]}-{times_tuple[2]} {times_tuple[3]}:{times_tuple[4]}:{times_tuple[5]}'
+    return currentTime
+
+def getTemperature():
+    sensor_temp = machine.ADC(4)
+    conversion_factor = 3.3 / (65535)
+    reading = sensor_temp.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    return temperature
+
+def getLightValue():
+    adc_light = ADC(Pin(28))
+    light_value = adc_light.read_u16()
+    return light_value
 
 while True:
     if btn.value():
+        #解決按下彈跳
         time.sleep_ms(50)
         if btn.value():
             is_press = True
             red_led.value(1)
     else:
+        #解決放開彈跳
         time.sleep_ms(50)
         if btn.value() == False:
             if is_press == True:     
-                print('release')
+                print('release')                
+                currentTime = getCurrentTime()
+                temperature= getTemperature()
+                light=getLightValue()
+                
                 is_press = False
-                '''
-                url_str = 'https://自已的網址/pico_w/2024-01-22 16:02:10?address=chicken_KFC&celsius=15.386'
+                
+                url_str = f'https://openapi-pico-jrib.onrender.com/pico_w/{currentTime}?address=chicken_KFC&celsius={temperature}&light={light}'
                 try:
                     response = urequests.get(url_str)
                     pass
@@ -34,7 +57,11 @@ while True:
                     else:
                         print("傳送失敗(server出現錯誤)")
                     response.close()
-                '''
+                
             
         
             red_led.value(0)
+        
+    
+        
+    
